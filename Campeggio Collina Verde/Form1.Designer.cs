e10ea0ae1449081a1565ef5d93f33402d9e7e38b
Fedefemia <10934171@itisrossi.vi.it>
2024-09-21 17:50:17 +0200

    o

---
+++ Campeggio Collina Verde/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Campeggio_Collina_Verde
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(334, 23);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(2);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(151, 20);
+            this.dateTimePicker1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(499, 22);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(63, 21);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Aggiungi";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(18, 23);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(184, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(18, 88);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(312, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(334, 89);
+            this.dateTimePicker2.Margin = new System.Windows.Forms.Padding(2);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(151, 20);
+            this.dateTimePicker2.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(499, 88);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(63, 20);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Rimuovi";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(18, 137);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(560, 206);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(206, 23);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(124, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Campeggio Collina Verde/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Campeggio_Collina_Verde
+{
+    public partial class Form1 : Form
+    {
+        private int postiliberi = 0;
+        private List<Posti> listaPosti = new List<Posti>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            dataGrid();
+        }
+
+        public class Posti
+        {
+            public string NomePosto { get; set; }
+            public string TargaPosto { get; set; }
+            public DateTime Data { get; set; }
+
+            public Posti(string nome, string tposto, DateTime date)
+            {
+                NomePosto = nome;
+                TargaPosto = tposto;
+                Data = date;
+            }
+        }
+        private void dataGrid()
+        {
+            dataGridView1.AutoGenerateColumns = false;
+            dataGridView1.ColumnCount = 3;
+
+            dataGridView1.Columns[0].Name = "NomePosto";
+            dataGridView1.Columns[0].HeaderText = "Nome Posto";
+            dataGridView1.Columns[0].DataPropertyName = "NomePosto";
+
+            dataGridView1.Columns[1].Name = "TargaPosto";
+            dataGridView1.Columns[1].HeaderText = "Targa Posto";
+            dataGridView1.Columns[1].DataPropertyName = "TargaPosto";
+
+            dataGridView1.Columns[2].Name = "Data";
+            dataGridView1.Columns[2].HeaderText = "Data";
+            dataGridView1.Columns[2].DataPropertyName = "Data";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (postiliberi < 20)
+            {
+                if (textBox1.Text != "" && textBox2.Text != "")
+                {
+                    Posti nuovoPosto = new Posti(textBox1.Text, textBox2.Text, dateTimePicker1.Value);
+                    listaPosti.Add(nuovoPosto);
+                    postiliberi++;
+                    textBox1.Clear();
+                    textBox2.Clear();
+                    dateTimePicker1.Value = DateTime.Now;
+                    aggiorna();
+                }
+            }
+            else
+            {
+                MessageBox.Show("I posti sono finiti");
+            }
+        }
+
+        private void aggiorna()
+        {
+            comboBox1.Items.Clear();
+            foreach (Posti posto in listaPosti)
+            {
+                comboBox1.Items.Add(posto.TargaPosto + " - " + posto.NomePosto);
+            }
+
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = listaPosti;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Posti foundPosto = null;
+            foreach (Posti posto in listaPosti)
+            {
+                string completa = posto.TargaPosto + " - " + posto.NomePosto;
+                if (completa == comboBox1.Text)
+                {
+                    int datetot = dateTimePicker2.Value.Day - posto.Data.Day;
+                    comboBox1.Items.Remove(completa);
+                    datetot *= 25;
+                    MessageBox.Show("Piazzola liberata con successo! costo: " + datetot);
+                    foundPosto = posto;
+                }
+            }
+
+            if (foundPosto != null)
+            {
+                listaPosti.Remove(foundPosto);
+                postiliberi--;
+                aggiorna();
+            }
+            comboBox1.Text = "";
+        }
+    }
+}
